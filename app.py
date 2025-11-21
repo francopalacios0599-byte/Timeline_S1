62936ff7bee7256043db508136e2c53cb445884c
francopalacios0599-byte <francopalacios0599@gmail.com>
2025-11-20 23:16:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ step=1
 st.image(imagenes[opcion], use_container_width=True)
 
 if opcion == 1:
-  st.info(" **1950 – Test de Turing** | Alan Turing propone un criterio para evaluar la inteligencia de una máquina.")
+  st.info(" **Estadio 1: Las Primeras Computadoras Electrónicas (Años 40 - 50)** | **Recursos Computacionales:** | •	Hardware: Tubos de vacío, relés electromecánicos, tambores magnéticos para almacenamiento. | •	Velocidad: Medida en milisegundos por operación.| •	Memoria: Muy limitada, a menudo unos pocos kilobytes.| •	Programación: Se realizaba directamente en lenguaje máquina o ensamblador, a través de tarjetas perforadas o cableado.|**Aplicaciones Soportadas:** |•	Cálculos científicos y militares complejos (como trayectorias balísticas o descifrado de códigos).|•	Procesamiento de datos muy básicos para censos o contabilidad a gran escala. |•	Simulaciones rudimentarias.")
 if opcion == 2:
   st.info(" **1956 – Nace el campo de la IA en Dartmouth** | John McCarthy acuña el término *Inteligencia Artificial*.")
 if opcion == 3:
